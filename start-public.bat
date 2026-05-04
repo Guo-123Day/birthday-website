0d43378b15fc62d51cf5683f0ed6089fcecb5321
Guo-123Day <754930514@qq.com>
2026-05-04 12:20:12 +0800

    add start-public.bat

---
+++ start-public.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+chcp 65001 >nul
+title Birthday Memorial Website
+echo.
+echo   Starting Birthday Memorial Website...
+echo.
+cd /d "%~dp0"
+start /b node server.js
+timeout /t 2 >nul
+npx localtunnel --port 3000
+pause
